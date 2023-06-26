7b08cda582a389fd3f09178dc48ab5529fabd921
ateet67 <ateet.p@shaligraminfotech.com>
2023-06-26 14:53:06 +0530

    updated!!

--- index.js
+++ index.js
@@ -5,21 +5,22 @@ const http = require('http');
 const server = http.createServer(app);
 const io = new Server(server);
 const port = process.env.PORT || 5000;
-  
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
-  
+
 io.on('connection', (socket) => {
+    console.log(socket.id);
     socket.on('send name', (username) => {
         io.emit('send name', (username));
     });
-  
+
     socket.on('send message', (chat) => {
         io.emit('send message', (chat));
     });
 });
-  
+
 server.listen(port, () => {
     console.log(`Server is listening at the port: ${port}`);
 });
